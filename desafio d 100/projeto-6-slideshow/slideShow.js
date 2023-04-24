a5e5b28039c3b3dafe8328c65ec6fc7fe6a384f8
Grazzielly <grazzisouza688@gmail.com>
2023-04-24 13:49:55 -0300

    Desafio dos 100 projetos

--- desafio d 100/projeto-6-slideshow/slideShow.js
+++ desafio d 100/projeto-6-slideshow/slideShow.js
@@ -9,7 +9,7 @@ const images = [
      { 'id' : '6' , 'url' :'./img/Koe no katachi.jpg' },
 ]
 
-const container = document.querySelector('#container-items');
+const containerItems = document.querySelector('#container-items');
 
 const loadImages = (images, container) => {
     images.forEach (image => {
@@ -17,10 +17,25 @@ const loadImages = (images, container) => {
          <div class='item'>
              <img src='${image.url}'
          </div>
-         
          `
         });
 
 }
 
-loadImages(images, container);
+loadImages(images, containerItems);
+
+let items = document.querySelectorAll('.item');
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore(lastItem, items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+    document.querySelector('#previous').addEventListener('click',previous);
+    document.querySelector('#next').addEventListener('click',next);
